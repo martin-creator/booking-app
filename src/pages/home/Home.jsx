9169557f5a2b33edbf3718ebf73af563e6283ea2
martin-creator <martinlubowa@outlook.com>
2023-01-21 23:26:29 +0300

    Added home css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import Featured from "../../components/featured/Featured"
 import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
 import "./home.css"
@@ -7,6 +8,13 @@ const Home = () => {
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList/>
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties/>
+      </div>
     </div>
   )
 }
