0bda53b1f7e2d5e7fc5533c990bd248557a8a6f3
martin-creator <martinlubowa@outlook.com>
2023-01-22 10:10:39 +0300

    Created mail list component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import Featured from "../../components/featured/Featured"
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
 import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
-import PropertyList from "../../components/propertyList/propertyList"
+import PropertyList from "../../components/propertyList/propertyList/propertyList"
 import "./home.css"
 
 const Home = () => {
