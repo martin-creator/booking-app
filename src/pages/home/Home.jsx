d2f32db5f380daf71318c06925d1b963d4e5f8bf
martin-creator <martinlubowa@outlook.com>
2023-01-22 09:03:25 +0300

    created featuredProperties

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Featured from "../../components/featured/Featured"
 import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
+import PropertyList from "../../components/propertyList/propertyList"
 import "./home.css"
 
 const Home = () => {
